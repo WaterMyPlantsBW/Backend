2003b92d9fa0ef9817a28e004dc31f1a5821aa31
adelazalewski <paliuadela@yahoo.com>
2021-01-27 20:24:51 -0500

    deploying app addjustments

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,12 @@ module.exports = {
       }
     }
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
+},
 testing: {
   client: 'sqlite3',
   useNullAsDefault: true,
