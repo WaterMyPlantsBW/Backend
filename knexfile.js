9b34316a3d994b46055ecd145d1331d39848a54d
adelazalewski <paliuadela@yahoo.com>
2021-02-01 20:44:21 -0500

    delopyment trying

--- knexfile.js
+++ knexfile.js
@@ -20,16 +20,16 @@ module.exports = {
       }
     }
   },
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL,
-  //   migrations: { directory: './data/migrations' },
-  //   seeds: { directory: './data/seeds' },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   }
-  // },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  },
   testing: {
       client: 'sqlite3',
       useNullAsDefault: true,
