471eeaab7472ea98456a1ab0d858d0e5ea3ae6a3
adelazalewski <paliuadela@yahoo.com>
2021-02-01 20:26:25 -0500

    deployment

--- knexfile.js
+++ knexfile.js
@@ -21,42 +21,40 @@ module.exports = {
     }
   },
   production: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: { directory: './data/migrations' },
     seeds: { directory: './data/seeds' },
     // pool: {
     //   min: 2,
     //   max: 10
-    // },
-    
-},
-// staging: {
-//   client: 'postgresql',
-//   connection: {
-//     database: 'plantsApp',
-//     user:     'user',
-//     password: 'password'
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-  
-// },
-testing: {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  connection: {
-    filename: '.data/testDB.db3'
   },
-  migrations: {
-    directory: "./data/migrations"
+  testing: {
+      client: 'sqlite3',
+      useNullAsDefault: true,
+      connection: {
+        filename: '.data/testDB.db3'
+      },
+      migrations: {
+        directory: "./data/migrations"
+      },
+      seeds: {
+        directory: "./data/seeds"
+      },
   },
-  seeds: {
-    directory: "./data/seeds"
-  },
-}
-  
 
-};
+  staging: {
+      client: 'postgresql',
+      connection: {
+        database: 'plantsApp',
+        user:     'user',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+    }
+}
+
+
