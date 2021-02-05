ec00b84d2c3096aa43502d4c5156dc9bd16dc815
adelazalewski <paliuadela@yahoo.com>
2021-02-05 15:18:40 -0500

    wrote some endpoint tests

---
+++ api/server.test.js
@@ -0,0 +1,19 @@
+
+const request = require("supertest")
+
+const server = require("./server")
+
+test("initial test", () => {
+    expect(2 + 2).toBe(4)
+})
+
+describe("test endpoints", () => {
+    test("can login", async () => {
+        const res = request(server).post("/auth/login").send({
+            username: "lambda",
+            password: "lambda"
+        })
+        expect(res.statusType).toBe(200)
+        expect(res.type).toBe("application/json")
+    })
+})
